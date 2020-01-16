dfc8e0d83f4d31ff79f52d3d6dd4f13cc21dce70
Christopher Pitt <cgpitt@gmail.com>
2020-01-16 06:39:16 +0200

    Update stream-to-stream.php

--- stream-to-stream.php
+++ stream-to-stream.php
@@ -9,10 +9,10 @@ $client = new Client();
 // get remote file stream
 $source = fopen(DOWNLOAD_URL, 'r');
 
-// open stream for local file
+// get local file stream
 $destination = fopen(LOCAL_FILE, 'w+');
 
-// copy from the guzzle stream to the local file stream
+// copy from the remote file stream to the local file stream
 stream_copy_to_stream($source, $destination);
 
 print getMemoryUsed() . " used while streaming source to destination\n";
